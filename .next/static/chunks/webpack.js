97103ddd7771ad36c81b66258e7070f9401db49c
Reny Brito da Costa <reny.dev03@gmail.com>
2026-01-06 20:03:59 +0000

    “api”

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "79e5df73d076a1e3"; }
+/******/ 		__webpack_require__.h = function() { return "80d053cd809b5cf5"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

de9e692e7c03c8becb3148a59b1887d906315485
Reny Brito da Costa <reny.dev03@gmail.com>
2026-01-12 01:39:22 +0000

    att api

--- pages/api/v1/status/index.js
+++ pages/api/v1/status/index.js
@@ -1,9 +1,36 @@
 import database from "infra/database.js";
+import { version } from "react";
 
 async function status(request, response) {
-  const result = await database.query("select 1 + 1 as sum");
-  console.log(result.rows);
-  response.status(200).json({Banana: "banana"});
+  const updateAt = new Date().toISOString();
+
+  const databaseVersionResult = await database.query("SHOW server_version;");
+  const databaseVersioValue = databaseVersionResult.rows[0].server_version;
+
+  const databaseMaxConnectionsResult = await database.query(
+    "SHOW max_connections;",
+  );
+  const databaseMaxConnectionsValue =
+    databaseMaxConnectionsResult.rows[0].max_connections;
+
+  const databaseName = process.env.POSTGRES_DB
+  const databaseOpenedConnectionsResult = await database.query({
+    text: "SELECT COUNT(*)::int FROM pg_stat_activity WHERE datname = $1;",
+    values: [databaseName]
+  });
+  const databaseOpenedConnectionsvalue =
+    databaseOpenedConnectionsResult.rows[0].count;
+
+  response.status(200).json({
+    update_at: updateAt,
+    dependecies: {
+      database: {
+        version: databaseVersioValue,
+        max_connections: parseInt(databaseMaxConnectionsValue),
+        opened_connections: parseInt(databaseOpenedConnectionsvalue),
+      },
+    },
+  });
 }
 
 export default status;
--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -1,5 +1,42 @@
-test("GEt to /api/v1/status should return 200", async () => {
+test("GET to /api/v1/status should return 200", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
-
+  
   expect(response.status).toBe(200);
+});
+
+test("GET to /api/v1/status should return update_at has a date", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  
+  expect(responseBody.update_at).toBeDefined()
+});
+
+test("GET to /api/v1/status should't return update_at has null", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  
+  const parsedUpdateAt = new Date(responseBody.update_at).toISOString()
+  
+  expect(responseBody.update_at).toEqual(parsedUpdateAt)
+});
+
+test("GET to /api/v1/status should return the version of postgres", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  
+  expect(responseBody.dependecies.database.version).toEqual("16.11")
+});
+
+test("GET to /api/v1/status should return the max_connection of postgres", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  
+  expect(responseBody.dependecies.database.max_connections).toEqual(100)
+});
+
+test("GET to /api/v1/status should return the opened_connection of postgres", async () => {
+  const response = await fetch("http://localhost:3000/api/v1/status");
+  const responseBody = await response.json();
+  
+  expect(responseBody.dependecies.database.opened_connections).toEqual(1)
 });

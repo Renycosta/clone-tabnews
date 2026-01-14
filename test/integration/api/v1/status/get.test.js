be1d1dea7e87cb9af50f2e57ae67ed2da2e28575
Reny Brito da Costa <reny.dev03@gmail.com>
2026-01-14 00:01:45 +0000

    teste migrations

--- test/integration/api/v1/status/get.test.js
+++ test/integration/api/v1/status/get.test.js
@@ -24,14 +24,14 @@ test("GET to /api/v1/status should return the version of postgres", async () => 
   const response = await fetch("http://localhost:3000/api/v1/status");
   const responseBody = await response.json();
   
-  expect(responseBody.dependecies.database.version).toEqual("16.11")
+  expect(responseBody.dependecies.database.version).toEqual("16.11 (f45eb12)")
 });
 
 test("GET to /api/v1/status should return the max_connection of postgres", async () => {
   const response = await fetch("http://localhost:3000/api/v1/status");
   const responseBody = await response.json();
   
-  expect(responseBody.dependecies.database.max_connections).toEqual(100)
+  expect(responseBody.dependecies.database.max_connections).toEqual(901)
 });
 
 test("GET to /api/v1/status should return the opened_connection of postgres", async () => {
